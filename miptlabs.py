44c8f2f230a23ee4853c94fd3bdd87d8ca5552bb
wiki-fan <uberslowpoke@gmail.com>
2017-09-09 23:00:37 +0300

    Major improvements, lab2.1

--- miptlabs.py
+++ miptlabs.py
@@ -20,25 +20,27 @@ class PQ:
         :param is_const: позволяет сделать константу, у которой не будет погрешностей.
             Иначе же за погрешность надо брать последнюю цифру (не реализовано).
         """
-        # if type(val) != u.quantities.Quantity:
-        #    raise TypeError("Only sympy units can be used")
+
         if dim is not None:
             self.dim = dim
+        elif PQ.is_simple_type(val):
+            self.dim = 1
         else:
-            self.dim = np.prod(val.args[1:])
+            self.dim = np.prod([elem for elem in val.args[1:] if type(elem) != u.dimensions.Dimension])
 
-        self.val = u.convert_to(val, dim)
+        self.val = u.convert_to(val, self.dim)
 
         if sigma is not None:
             self.sigma = u.convert_to(sigma, dim)
-            self.epsilon = u.convert_to(self.sigma / self.val,
-                                        sp.numbers.Integer(1))
+            self.epsilon = u.convert_to(self.sigma/self.val, sp.numbers.Integer(1))
         elif epsilon is not None:
             self.epsilon = u.convert_to(epsilon, sp.numbers.Integer(1))
-            self.sigma = u.convert_to(self.val * self.epsilon, dim)
+            self.sigma = u.convert_to(self.val*self.epsilon, dim)
         else:
             if is_const:
-                self.sigma = self.epsilon = 0  # TODO:last digit
+                self.sigma = self.epsilon = 0
+            else:
+                pass  # TODO:last digit
 
         if symbol is None:
             if not hasattr(PQ, 'symbol_counter'):
@@ -49,16 +51,21 @@ class PQ:
         else:
             self.symbol = sp.symbols(symbol)
 
-    # TODO: use internal dim information
-    def repr_as(self, dim=None):
+    def repr_as(self, dim):
+        self.dim = dim
+        self.val = u.convert_to(self.val, dim).n()
+        self.sigma = u.convert_to(self.sigma, dim).n()
+        return self
+
+    def str_as(self, dim=None):
         if dim is None:
             dim = self.dim
 
-        return "%f±%f %s (%f)" % (
-            float(u.convert_to(self.val, dim).n() / dim),
-            float(u.convert_to(self.sigma, dim).n() / dim),
+        return "%f±%f %s (%f)"%(
+            float(u.convert_to(self.val, dim).n()/dim),
+            float(u.convert_to(self.sigma, dim).n()/dim),
             dim,
-            float(u.convert_to(self.epsilon, sp.numbers.Integer(1))))
+            float(u.convert_to(self.epsilon, 1)))
 
     def raw_print(self):
         print(self.val)
@@ -66,7 +73,7 @@ class PQ:
         print(self.epsilon)
 
     def __str__(self):
-        return self.repr_as(self.dim)
+        return self.str_as(self.dim)
 
     def __repr__(self):
         return self.__str__()
@@ -76,8 +83,8 @@ class PQ:
         if dim is None:
             dim = self.dim
 
-        float_val = float(u.convert_to(self.val, dim).n() / dim)
-        float_sigma = float(u.convert_to(self.sigma, dim).n() / dim)
+        float_val = float(u.convert_to(self.val, dim).n()/dim)
+        float_sigma = float(u.convert_to(self.sigma, dim).n()/dim)
         float_percents = float(u.convert_to(self.epsilon, sp.numbers.Integer(1)))*100
 
         # print(float_val, float_sigma, float_percents)
@@ -91,7 +98,7 @@ class PQ:
         # Если первые значащие цифры погрешности 1 или 2, оставляем 2 цифры, иначе 1
         msd = most_significant_digit(float_sigma)
         num_sign_dig = None
-        if float_sigma / 10**(msd - 2) < 30:
+        if float_sigma/10**(msd - 2) < 30:
             num_sign_dig = 2
         else:
             num_sign_dig = 1
@@ -99,17 +106,54 @@ class PQ:
         # print(num_sign_dig)
         # msd_percents = most_significant_digit(float_epsilon) TODO: решить, что делать здесь
         # print(msd_percents)
-        return '%*.*f±%*.*f %s (%f%%)' % (
+        return '%*.*f±%*.*f %s (%f%%)'%(
             num_sign_dig if msd - num_sign_dig >= 0 else msd,
             0 if msd - num_sign_dig >= 0 else num_sign_dig - msd,
-            round(float_val / 10**(msd - num_sign_dig)) * 10**(msd - num_sign_dig),
+            round(float_val/10**(msd - num_sign_dig))*10**(msd - num_sign_dig),
             num_sign_dig if msd - num_sign_dig >= 0 else msd,
             0 if msd - num_sign_dig >= 0 else num_sign_dig - msd,
-            round(float_sigma / 10**(msd - num_sign_dig)) * 10**(msd - num_sign_dig),
+            round(float_sigma/10**(msd - num_sign_dig))*10**(msd - num_sign_dig),
             dim,
             float_percents)
 
-# TODO: автоустановление dim по dim'ам величин
+    def __add__(self, other):
+        return eval(self.dim, lambda self, other: self + other, self, other)
+
+    def __radd__(self, other):
+        return eval(self.dim, lambda self, other: self + other, self, other)
+
+    def __sub__(self, other):
+        return eval(self.dim, lambda self, other: self - other, self, other)
+
+    def __rsub__(self, other):
+        return eval(self.dim, lambda self, other: self - other, self, other)
+
+    def __mul__(self, other):
+        return eval(self.dim if PQ.is_simple_type(other) else self.dim*other.dim,
+                    lambda self, other: self*other, self, other)
+
+    def __rmul__(self, other):
+        return eval(self.dim if PQ.is_simple_type(other) else self.dim*other.dim,
+                    lambda self, other: self*other, self, other)
+
+    def __truediv__(self, other):
+        return eval(self.dim if PQ.is_simple_type(other) else self.dim/other.dim,
+                    lambda self, other: self/other, self, other)
+
+    def __rtruediv__(self, other):
+        return eval(1/self.dim if PQ.is_simple_type(other) else other.dim/self.dim,
+                    lambda self, other: other/self, self, other)
+
+    def __pow__(self, power, modulo=None):
+        return eval(self.dim**power if PQ.is_simple_type(power) else self.dim**power.dim,
+                    lambda self, other: self**power, self, power)
+
+    def is_simple_type(arg):
+        return type(arg) is int or type(arg) is float or type(arg) is np.float64 \
+               or type(arg) is u.quantities.Quantity #or type(arg) is sympy.Mul
+        #return type(arg) is not PQ and type(arg) is not np.ndarray
+
+
 # TODO: Узнавать, какая величина давала наибольший вклад в погрешность.
 def eval(dim, lambd, *args, symbol=None):
     """
@@ -122,45 +166,76 @@ def eval(dim, lambd, *args, symbol=None):
     :param symbol: можно особо указать sympy символ (зачем?)
     :return: Новое PQ.
     """
+    args = list(args)
+    log.debug('args: %s', args)
+
+    for i in range(len(args)):
+        if PQ.is_simple_type(args[i]):
+            args[i] = PQ(args[i], is_const=True)
+    log.debug('args converted to PQ: %s', args)
+
     partdiffs = [sp.diff(lambd(*[arg.symbol for arg in args]), x.symbol) for x
                  in args]
     log.debug('partdiffs %s', partdiffs)
+
     values = {(arg.symbol, arg.val) for arg in args}
     log.debug('values of partdiffs %s', values)
-    summands = [sp.Abs(partdiff.subs(values).evalf()**2 * pe.sigma**2) for
+
+    summands = [sp.Abs(partdiff.subs(values).evalf()**2*pe.sigma**2) for
                 (partdiff, pe) in zip(partdiffs, args)]
     summands = [u.convert_to(s, dim**2) for s in summands]
     log.debug('summands %s', summands)
+
     new_val = u.convert_to(lambd(*[arg.val for arg in args]), dim)
     log.debug('val %s', new_val)
+
     new_sigma = u.convert_to(sp.sqrt(sum(summands)), dim)
     log.debug('sigma %s', new_sigma)
+
     return PQ(new_val, sigma=new_sigma, symbol=symbol, dim=dim)
 
 
-def plt_pq(grid_exper, exper=None, grid_theor=None, theor=None):
+def plt_pq(grid, values, label=None, color=None, ols=False):
     """
-    Строит графики. Рассчитана на график экспериментальных и теоретических значений, с крестами погрешностей.
-    По умолчанию используется grid_exper, если теоретических данных другое количество, надо задавать grid_theor.
+    Строит графики. С крестами погрешностей. ols=True рисует ещё и прямую, приближающую значения по НМК.
     Вызовы plt.figure и plt.show должны быть снаружи.
     Можно добавлять подписи к осям и прочее.
     """
+
     def get(lambd, arr):
         return np.array([lambd(elem) for elem in arr])
 
-    if grid_theor is None:
-        grid_theor = grid_exper
+    vals = get(lambda elem: elem.val, values)
+    sigmas = get(lambda elem: elem.sigma, values)
 
-    for pqs, color, label, grid in ((exper, 'red', 'Экспериментальная', grid_exper),
-                                    (theor, 'blue', 'Расчётная', grid_theor)):
-        if pqs is not None:
-            vals = get(lambda elem: elem.val, pqs)
-            sigmas = get(lambda elem: elem.sigma, pqs)
+    y = (vals/values[0].dim).astype(float)
+    y_s = (sigmas/values[0].dim).astype(float)
+    line = plt.plot(grid, y, color=color, label=label, zorder=1)
+    plt.errorbar(grid, y, xerr=0, yerr=y_s, color=line[0].get_color(), zorder=1)
+    plt.scatter(grid, y, color=line[0].get_color(), zorder=2)
 
-            y = (vals / u.hz).astype(float)
-            y_s = (sigmas / u.hz).astype(float)
-            plt.plot(grid, y, color=color, label=label)
-            plt.errorbar(grid, y, xerr=0, yerr=y_s, color=color)
-            plt.scatter(grid, y, color=color)
+    if ols == True:
+        ols_coefs, ols_errors = OLS(grid, values)
+        x1 = grid[0]-1
+        x2 = grid[-1] + 1
+        y1 = ols_coefs[0]*x1 + ols_coefs[1]
+        y2 = ols_coefs[0]*x2 + ols_coefs[1]
+        plt.plot([x1, x2], [y1, y2], color='black',
+                 linestyle='dashed', zorder=3,
+                 label='OLS for %s'%label if label is not None else None)
+
     plt.grid()
     plt.legend()
+
+
+def OLS(x, y):
+    if type(y[0]) is PQ:
+        y = np.array([float(pq.val/y[0].dim) for pq in y])
+    coefs = np.polyfit(x, y, deg=1)
+    sigma_b = 1/np.sqrt(len(y))*np.sqrt((np.mean(y**2)-np.mean(y)**2)/(np.mean(x**2)-np.mean(x)**2)-coefs[0]**2)
+    sigma_a = sigma_b*(np.mean(x**2)-np.mean(x)**2)
+    errors = [sigma_b, sigma_a]
+    return coefs, errors
+
+def celsium_to_kelvins(c):
+    return (c+273.15)*u.kelvins
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from miptlabs import *
+import miptlabs as ml
 
 
 class TestPE(unittest.TestCase):
@@ -13,6 +14,19 @@ class TestPE(unittest.TestCase):
         d = PQ(0.4*u.m, sigma=0.03*u.m, dim=u.m)
         print(d.repr_rounded_as(u.m))
 
+    def test_opers(self):
+        a = ml.PQ(1*u.m, dim=u.m, sigma=1*u.mm)
+        b = ml.PQ(2*u.m, dim=u.m, sigma=1*u.mm)
+        c = ml.PQ(2*u.m, dim=u.m, sigma=1*u.mm)
+        # type(u.m)
+        # u.convert_to(a.sigma, a.dim)
+        ml.eval(a.dim, lambda a, b: a + b, a, b)
+        # a+b #3.000000±0.001414 meter (0.000471)
+        # (a*3) # 3.000000±0.003000 meter (0.001000)
+        # a*b # 2.000000±0.002236 meter**2 (0.001118)
+        # a/b # 0.500000±0.000559 1 (0.001118)
+        # 1/b # 0.500000±0.000250 1/meter (0.000500)
+        # type(u.m.args[1])
 
 if __name__ == '__main__':
     unittest.main()
